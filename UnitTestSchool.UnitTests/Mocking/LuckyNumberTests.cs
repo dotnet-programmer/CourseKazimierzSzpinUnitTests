74f05ee9b6cc29e0d79237285a3fd441fefb940a
dotnet-programmer <26222619+dotnet-programmer@users.noreply.github.com>
2023-09-04 18:23:09 +0200

    Tests

--- UnitTestSchool.UnitTests/Mocking/LuckyNumberTests.cs
+++ UnitTestSchool.UnitTests/Mocking/LuckyNumberTests.cs
@@ -7,8 +7,8 @@ internal class LuckyNumberTests
 	//private Mock<IMyDateTimeWrapper> _mockMyDateTimeWrapper;
 	//private Mock<IMessageBoxWrapper> _mockMessageBoxWrapper;
 	//private TaskService _taskService;
-	private RandomNumber _randomNumber;
+	//private RandomNumber _randomNumber;
 
-	[SetUp]
-	public void SetUp() => _randomNumber = new RandomNumber { Number =  };
+	//[SetUp]
+	//public void SetUp() => _randomNumber = new RandomNumber { Number =  };
 }
